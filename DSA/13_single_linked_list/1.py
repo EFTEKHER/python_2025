0326c1cba08237f558367a4cfaf6cc500b392812
EFTEKHER2000 <eftekherali2000@gmail.com>
2025-09-19 01:04:44 +0600

    done

--- DSA/13_single_linked_list/1.py
+++ DSA/13_single_linked_list/1.py
@@ -18,7 +18,44 @@ class LinkedList:
         self.length+=1  
         print(f"Appended {value} to the list.")    
         print(f"Current list length: {self.length}")
+    def __str__(self):
+        temp=self.head
+        result=""
+        while temp is not None:
+            result += str(temp.value) + " -> "
+            temp=temp.next
         
+        return result
+    def insertathead(self,value):
+        temp=self.head
+        
+        new_node=Node(value)
+        
+        new_node.next=temp
+        
+        self.head=new_node
+        
+        self.length+=1
+        
+    def insertatindex(self,index,value):
+        new_node=Node(value)
+        if self.length==0:
+            self.head=new_node
+            self.tail=new_node
+        elif index==0:
+            self.insertathead(value)
+        elif index<-1 or index>self.length:
+            return None
+        else:
+            temp=self.head
+            for _ in range(index-1):
+                prevnode=temp.next
+                temp=prevnode
+                nextnode=temp.next     
+                temp.next=new_node
+                new_node.next=nextnode
+        self.length+=1
+
 if __name__=="__main__":
     ll=LinkedList()
     ll.append(10)
@@ -26,9 +63,8 @@ if __name__=="__main__":
     ll.append(30)
     ll.append(40)
     ll.append(50)
+    ll.insertathead(16)
+    ll.insertatindex(3,25)
+    ll.insertatindex(0,5)
     # Print the linked list
-    current=ll.head
-    while current:
-        print(current.value, end=" -> ")
-        current=current.next
-    print("None")        
+    print(ll)
